de4ff1c81784214eff7b07d50d06569c540b02dd
Nguyen Duc Tai <Tai.NguyenDuc2@vn.bosch.com>
2025-12-10 18:11:14 +0700

    Delete old approach

--- src/main/java/com/autosearching/service/SearchService.java
+++
@@ -1,8 +0,0 @@
-package com.autosearching.service;
-
-import org.openqa.selenium.WebDriver;
-
-public interface SearchService {
-
-    boolean executeAutomationSearching(String hotelName, String region, int numberPageWillFind) throws InterruptedException;
-}
